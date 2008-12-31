154d3009c11879075649627b94a2d01082e16ce2
chromakode <chromakode@gmail.com>
2008-12-31 00:36:34 -0800

    Add skeleton buildxpi_config_local, switch to https for extension URLs

---
+++ buildxpi_config_local.py
@@ -0,0 +1,13 @@
+from buildxpi import run_spock
+ 
+config = {
+ 
+    "after": [run_spock(
+                       spock_path = "[insert spock location here]",
+                       key_dir_path = "[something like .mozilla/mccoy/[...]/",
+                       destination_url = "https://addons.reddit.com/socialite/socialite.xpi",
+                       input_path = "update.rdf-base",
+                       output_path = "update.rdf",
+                   )]
+ 
+}
