a43665faa2d740b3c4d1d8f617a12a85ad7314ba
chromakode <chromakode@gmail.com>
2009-01-30 01:20:53 -0800

    Wrap global JS files in functions to fix namespace pollution.

--- content/socialitePreferences.js
+++ content/socialitePreferences.js
@@ -1,153 +1,158 @@
-let modules = {};
-let importModule = function(name) Components.utils.import(name, modules);
-
-let Socialite = importModule("resource://socialite/socialite.jsm").Socialite;
-let faviconWatch = importModule("resource://socialite/utils/faviconWatch.jsm");
-let domUtils = importModule("resource://socialite/utils/domUtils.jsm");
-
-let observerService = Components.classes["@mozilla.org/observer-service;1"]
-                                         .getService(Components.interfaces.nsIObserverService);
-
-let promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                       .getService(Components.interfaces.nsIPromptService); 
-
-var SocialiteSitePreferences = {
-
-  load: function SSPrefs_load() {
-    this.strings = document.getElementById("socialitePreferencesStrings");
-    this.siteListbox = document.getElementById("socialiteSiteListbox"); 
-    this.siteListbox.addSite = function(site) {
-      let newItem = document.createElement("listitem");
-      newItem.value = site.siteID;
-      
-      let siteCell = document.createElement("listcell");
-      newItem.appendChild(siteCell);
-      let urlCell = document.createElement("listcell");
-      newItem.appendChild(urlCell);
-      
-      newItem.update = function() {
-        siteCell.setAttribute("class", "listcell-iconic");
+var SocialiteSitePreferences = (function() {
+  let modules = {};
+  let importModule = function(name) Components.utils.import(name, modules);
+  
+  let Socialite = importModule("resource://socialite/socialite.jsm").Socialite;
+  let faviconWatch = importModule("resource://socialite/utils/faviconWatch.jsm");
+  let domUtils = importModule("resource://socialite/utils/domUtils.jsm");
+  
+  let observerService = Components.classes["@mozilla.org/observer-service;1"]
+                                           .getService(Components.interfaces.nsIObserverService);
+  
+  let promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                         .getService(Components.interfaces.nsIPromptService); 
+  
+  var SocialiteSitePreferences = {
+  
+    load: function SSPrefs_load() {
+      this.strings = document.getElementById("socialitePreferencesStrings");
+      this.siteListbox = document.getElementById("socialiteSiteListbox"); 
+      this.siteListbox.addSite = function(site) {
+        let newItem = document.createElement("listitem");
+        newItem.value = site.siteID;
         
-        if (siteCell.getAttribute("label") != site.siteName) {
-          siteCell.setAttribute("label", site.siteName);
-          domUtils.insertListboxSorted(newItem, SocialiteSitePreferences.siteListbox, function(item1, item2) {
-            let label1 = item1.firstChild.getAttribute("label"); 
-            let label2 = item2.firstChild.getAttribute("label"); 
-            return label1.localeCompare(label2);
-          });
+        let siteCell = document.createElement("listcell");
+        newItem.appendChild(siteCell);
+        let urlCell = document.createElement("listcell");
+        newItem.appendChild(urlCell);
+        
+        newItem.update = function() {
+          siteCell.setAttribute("class", "listcell-iconic");
+          
+          if (siteCell.getAttribute("label") != site.siteName) {
+            siteCell.setAttribute("label", site.siteName);
+            domUtils.insertListboxSorted(newItem, SSPrefs.siteListbox, function(item1, item2) {
+              let label1 = item1.firstChild.getAttribute("label"); 
+              let label2 = item2.firstChild.getAttribute("label"); 
+              return label1.localeCompare(label2);
+            });
+          }
+          
+          if (newItem.removeFaviconWatch) { newItem.removeFaviconWatch(); }
+          newItem.removeFaviconWatch = faviconWatch.useFaviconAsAttribute(siteCell, "image", site.siteURL);
+          
+          urlCell.setAttribute("label", site.siteURL);
+        };
+        
+        newItem.update();
+      }
+      this.siteListbox.getItemBySiteID = function(siteID) {
+        for (let i=0; i<this.childNodes.length; i++) {
+          if (this.childNodes[i].value == siteID)
+            return this.childNodes[i];
         }
-        
-        if (newItem.removeFaviconWatch) { newItem.removeFaviconWatch(); }
-        newItem.removeFaviconWatch = faviconWatch.useFaviconAsAttribute(siteCell, "image", site.siteURL);
-        
-        urlCell.setAttribute("label", site.siteURL);
-      };
-      
-      newItem.update();
-    }
-    this.siteListbox.getItemBySiteID = function(siteID) {
-      for (let i=0; i<this.childNodes.length; i++) {
-        if (this.childNodes[i].value == siteID)
-          return this.childNodes[i];
+        return null;
       }
-      return null;
-    }
-    this.siteListbox.removeSite = function(siteID) {
-      let item = this.getItemBySiteID(siteID);
-      if (item) {
+      this.siteListbox.removeSite = function(siteID) {
+        let item = this.getItemBySiteID(siteID);
+        if (item) {
+          if (item.removeFaviconWatch) { item.removeFaviconWatch(); }
+          this.removeChild(item);
+        }
+      }
+      
+      // Populate the list
+      for (let [siteID, site] in Socialite.sites) {
+        this.siteListbox.addSite(site);
+      }
+      
+      // Set the minimum of the refresh interval textbox
+      let refreshIntervalTextbox = document.getElementById("refreshIntervalTextbox");
+      refreshIntervalTextbox.min = Math.ceil(Socialite.globals.MINIMUM_REFRESH_INTERVAL / 60);
+      
+      observerService.addObserver(this.siteObserver, "socialite-load-site", false);
+      observerService.addObserver(this.siteObserver, "socialite-unload-site", false);
+    },
+    
+    unload: function SSPrefs_unload() {
+      Array.map(this.siteListbox.childNodes, function(item) {
         if (item.removeFaviconWatch) { item.removeFaviconWatch(); }
-        this.removeChild(item);
-      }
-    }
-    
-    // Populate the list
-    for (let [siteID, site] in Socialite.sites) {
-      this.siteListbox.addSite(site);
-    }
-    
-    // Set the minimum of the refresh interval textbox
-    let refreshIntervalTextbox = document.getElementById("refreshIntervalTextbox");
-    refreshIntervalTextbox.min = Math.ceil(Socialite.globals.MINIMUM_REFRESH_INTERVAL / 60);
-    
-    observerService.addObserver(this.siteObserver, "socialite-load-site", false);
-    observerService.addObserver(this.siteObserver, "socialite-unload-site", false);
-  },
-  
-  unload: function SSPrefs_unload() {
-    Array.map(this.siteListbox.childNodes, function(item) {
-      if (item.removeFaviconWatch) { item.removeFaviconWatch(); }
-    });
-    
-    observerService.removeObserver(this.siteObserver, "socialite-load-site");
-    observerService.removeObserver(this.siteObserver, "socialite-unload-site");
-  },
-  
-  siteObserver: {
-    observe: function(subject, topic, data) {
-      if (topic == "socialite-load-site") {
-        SocialiteSitePreferences.siteListbox.addSite(Socialite.sites.byID[data]);
-      } else if (topic == "socialite-unload-site") {
-        SocialiteSitePreferences.siteListbox.removeSite(data);
-      }
-    }
-  },
-
-  siteAdd: function SSPrefs_siteAdd(event) {
-    let newSiteInfo = {};
-    let dialog = document.documentElement.openSubDialog("chrome://socialite/content/socialiteSiteProperties.xul", "", {
-      isNewSite: true,
-      newSiteInfo: newSiteInfo
-    });
-    
-    this.site = Socialite.sites.createSite(newSiteInfo["siteClassID"], newSiteInfo["siteID"], newSiteInfo["siteName"], newSiteInfo["siteURL"])
-  },
-  
-  siteProperties: function SSPrefs_siteProperties(event) {
-    let item = this.siteListbox.selectedItem;
-    if (item) {
-      let site = Socialite.sites.byID[item.value];
-      let dialog = document.documentElement.openSubDialog("chrome://socialite/content/socialiteSiteProperties.xul", "", {
-        isNewSite: false, 
-        site: site
       });
-      item.update();
-    }
-  },
-  
-  siteRemove: function SSPrefs_siteRemove(event) {
-    let item = this.siteListbox.selectedItem;
-    if (item) {
-      let site = Socialite.sites.byID[item.value];
       
-      let confirmed = promptService.confirm(window,
-          this.strings.getString("removeSiteConfirm.title"),
-          this.strings.getFormattedString("removeSiteConfirm.message", [ site.siteName ])
-      );
-      
-      if (confirmed) {      
-        Socialite.sites.deleteSite(site);
-        Socialite.sites.saveConfiguredSites();
+      observerService.removeObserver(this.siteObserver, "socialite-load-site");
+      observerService.removeObserver(this.siteObserver, "socialite-unload-site");
+    },
+    
+    siteObserver: {
+      observe: function(subject, topic, data) {
+        if (topic == "socialite-load-site") {
+          SSPrefs.siteListbox.addSite(Socialite.sites.byID[data]);
+        } else if (topic == "socialite-unload-site") {
+          SSPrefs.siteListbox.removeSite(data);
+        }
       }
+    },
+  
+    siteAdd: function SSPrefs_siteAdd(event) {
+      let newSiteInfo = {};
+      let dialog = document.documentElement.openSubDialog("chrome://socialite/content/socialiteSiteProperties.xul", "", {
+        isNewSite: true,
+        newSiteInfo: newSiteInfo
+      });
+      
+      this.site = Socialite.sites.createSite(newSiteInfo["siteClassID"], newSiteInfo["siteID"], newSiteInfo["siteName"], newSiteInfo["siteURL"])
+    },
+    
+    siteProperties: function SSPrefs_siteProperties(event) {
+      let item = this.siteListbox.selectedItem;
+      if (item) {
+        let site = Socialite.sites.byID[item.value];
+        let dialog = document.documentElement.openSubDialog("chrome://socialite/content/socialiteSiteProperties.xul", "", {
+          isNewSite: false, 
+          site: site
+        });
+        item.update();
+      }
+    },
+    
+    siteRemove: function SSPrefs_siteRemove(event) {
+      let item = this.siteListbox.selectedItem;
+      if (item) {
+        let site = Socialite.sites.byID[item.value];
+        
+        let confirmed = promptService.confirm(window,
+            this.strings.getString("removeSiteConfirm.title"),
+            this.strings.getFormattedString("removeSiteConfirm.message", [ site.siteName ])
+        );
+        
+        if (confirmed) {      
+          Socialite.sites.deleteSite(site);
+          Socialite.sites.saveConfiguredSites();
+        }
+      }
+    },
+    
+    refreshIntervalEnabledFromPreference: function SSPrefs_refreshIntervalEnabledFromPreference() {
+      let pref = document.getElementById("prefRefreshIntervalEnabled");
+      let textbox = document.getElementById("refreshIntervalTextbox");
+      textbox.disabled = !pref.value;
+      return pref.value;
+    },
+    
+    refreshIntervalFromPreference: function SSPrefs_refreshIntervalFromPreference() {
+      let pref = document.getElementById("prefRefreshInterval");
+      let seconds = pref.value;
+      return Math.ceil(seconds / 60);
+    },
+    
+    refreshIntervalToPreference: function SSPrefs_refreshIntervalToPreference() {
+      let textbox = document.getElementById("refreshIntervalTextbox");
+      let minutes = textbox.valueNumber;
+      return minutes*60;
     }
-  },
+    
+  };
   
-  refreshIntervalEnabledFromPreference: function SSPrefs_refreshIntervalEnabledFromPreference() {
-    let pref = document.getElementById("prefRefreshIntervalEnabled");
-    let textbox = document.getElementById("refreshIntervalTextbox");
-    textbox.disabled = !pref.value;
-    return pref.value;
-  },
-  
-  refreshIntervalFromPreference: function SSPrefs_refreshIntervalFromPreference() {
-    let pref = document.getElementById("prefRefreshInterval");
-    let seconds = pref.value;
-    return Math.ceil(seconds / 60);
-  },
-  
-  refreshIntervalToPreference: function SSPrefs_refreshIntervalToPreference() {
-    let textbox = document.getElementById("refreshIntervalTextbox");
-    let minutes = textbox.valueNumber;
-    return minutes*60;
-  }
-
-};
+  var SSPrefs = SocialiteSitePreferences;
+  return SSPrefs;
+})();
--- content/socialiteSiteProperties.js
+++ content/socialiteSiteProperties.js
@@ -1,159 +1,163 @@
-let modules = {};
-let importModule = function(name) Components.utils.import(name, modules);
-
-let Socialite = importModule("resource://socialite/socialite.jsm").Socialite;
-let SiteClassRegistry = importModule("resource://socialite/site.jsm").SiteClassRegistry;
-
-let IOService = Components.classes["@mozilla.org/network/io-service;1"]
-                .getService(Components.interfaces.nsIIOService);
-
-var SocialiteSiteProperties = {
-
-  init: function SSProps_init() {
-    this.prefWindow = document.getElementById("socialiteSiteProperties");
-    this.buttonAccept = this.prefWindow._buttons.accept;
-    
-    this.strings = document.getElementById("socialiteSitePropertiesStrings");
+var SocialiteSiteProperties = (function() {
+  let modules = {};
+  let importModule = function(name) Components.utils.import(name, modules);
   
-    this.isNewSite = window.arguments[0].isNewSite;
-    if (this.isNewSite) {
-      this.siteID = Socialite.sites.requestID();
-    } else {
-      this.site = window.arguments[0].site;
-      this.siteID = this.site.siteID;
-    }
-    this.newSiteInfo = window.arguments[0].newSiteInfo;
-    
-    // Set up preferences for the site
-    this.preferences = document.getElementById("preferencesSocialite");
-    
-    // Site name and URL preferences
-    this.textboxSiteName = document.getElementById("textboxSiteName");    
-    this.textboxSiteURL = document.getElementById("textboxSiteURL");
-    this.labelSiteURLError = document.getElementById("labelSiteURLError");
-    
-    this.prefSiteName = this._addBaseSitePreference("prefSiteName", "siteName", "string");
-    this.prefSiteURL = this._addBaseSitePreference("prefSiteURL", "siteURL", "string");
-    
-    // Site class dropdown menu initialization (populate)
-    let buttonSiteClass = document.getElementById("buttonSiteClass");
-    let menuSiteClass = document.getElementById("menuSiteClass");
-    for each (let siteClass in SiteClassRegistry.classes) {
-      let menuItem = document.createElement("menuitem");
+  let Socialite = importModule("resource://socialite/socialite.jsm").Socialite;
+  let SiteClassRegistry = importModule("resource://socialite/site.jsm").SiteClassRegistry;
+  
+  let IOService = Components.classes["@mozilla.org/network/io-service;1"]
+                  .getService(Components.interfaces.nsIIOService);
+  
+  var SocialiteSiteProperties = {
+  
+    init: function SSProps_init() {
+      this.prefWindow = document.getElementById("socialiteSiteProperties");
+      this.buttonAccept = this.prefWindow._buttons.accept;
       
-      menuItem.setAttribute("class", "menuitem-iconic");
-      menuItem.setAttribute("label", siteClass.prototype.siteClassName);
-      menuItem.setAttribute("image", siteClass.prototype.siteClassIconURI);
-      menuItem.setAttribute("value", siteClass.prototype.siteClassID);
+      this.strings = document.getElementById("socialiteSitePropertiesStrings");
+    
+      this.isNewSite = window.arguments[0].isNewSite;
+      if (this.isNewSite) {
+        this.siteID = Socialite.sites.requestID();
+      } else {
+        this.site = window.arguments[0].site;
+        this.siteID = this.site.siteID;
+      }
+      this.newSiteInfo = window.arguments[0].newSiteInfo;
       
-      menuSiteClass.appendChild(menuItem);
-    }
-       
-    // Handler to update preferences pane for site class
-    this.boxSiteProperties = document.getElementById("boxSiteProperties");
-    buttonSiteClass.addEventListener("ValueChange", function(event) {
-      let container = SSP.boxSiteProperties;
+      // Set up preferences for the site
+      this.preferences = document.getElementById("preferencesSocialite");
       
-      // Remove old pane
-      if (container.hasChildNodes()) {
-        container.removeChild(container.firstChild);
+      // Site name and URL preferences
+      this.textboxSiteName = document.getElementById("textboxSiteName");    
+      this.textboxSiteURL = document.getElementById("textboxSiteURL");
+      this.labelSiteURLError = document.getElementById("labelSiteURLError");
+      
+      this.prefSiteName = this._addBaseSitePreference("prefSiteName", "siteName", "string");
+      this.prefSiteURL = this._addBaseSitePreference("prefSiteURL", "siteURL", "string");
+      
+      // Site class dropdown menu initialization (populate)
+      let buttonSiteClass = document.getElementById("buttonSiteClass");
+      let menuSiteClass = document.getElementById("menuSiteClass");
+      for each (let siteClass in SiteClassRegistry.classes) {
+        let menuItem = document.createElement("menuitem");
+        
+        menuItem.setAttribute("class", "menuitem-iconic");
+        menuItem.setAttribute("label", siteClass.prototype.siteClassName);
+        menuItem.setAttribute("image", siteClass.prototype.siteClassIconURI);
+        menuItem.setAttribute("value", siteClass.prototype.siteClassID);
+        
+        menuSiteClass.appendChild(menuItem);
+      }
+         
+      // Handler to update preferences pane for site class
+      this.boxSiteProperties = document.getElementById("boxSiteProperties");
+      buttonSiteClass.addEventListener("ValueChange", function(event) {
+        let container = SSProps.boxSiteProperties;
+        
+        // Remove old pane
+        if (container.hasChildNodes()) {
+          container.removeChild(container.firstChild);
+        }
+        
+        // Add new pane and reset preferences
+        let siteClassID = buttonSiteClass.selectedItem.value;
+        SSProps.siteClass = SiteClassRegistry.getClass(siteClassID);
+        
+        Socialite.sites.setSiteDefaultPreferences(SSProps.siteID, SSProps.siteClass);
+        let pane = SSProps.siteClass.prototype.createPreferencesUI(document, SSProps);
+        container.appendChild(pane);
+      }, false);
+      
+      // Site class preference initialization
+      this.prefSiteClassID = this._addBaseSitePreference("prefSiteClassID", "siteClassID", "string");
+      if (this.isNewSite) {
+        this.prefSiteClassID.value = menuSiteClass.childNodes.item(0).value;
+      } else {
+        this.prefSiteClassID.disabled = true; 
       }
       
-      // Add new pane and reset preferences
-      let siteClassID = buttonSiteClass.selectedItem.value;
-      SSP.siteClass = SiteClassRegistry.getClass(siteClassID);
-      
-      Socialite.sites.setSiteDefaultPreferences(SSP.siteID, SSP.siteClass);
-      let pane = SSP.siteClass.prototype.createPreferencesUI(document, SSP);
-      container.appendChild(pane);
-    }, false);
+      // Initial validation and form setup
+      this.validate();
+    },
     
-    // Site class preference initialization
-    this.prefSiteClassID = this._addBaseSitePreference("prefSiteClassID", "siteClassID", "string");
-    if (this.isNewSite) {
-      this.prefSiteClassID.value = menuSiteClass.childNodes.item(0).value;
-    } else {
-      this.prefSiteClassID.disabled = true; 
-    }
+    _addBaseSitePreference: function SSProps_addBaseSitePreference(prefID, prefName, prefType) {
+      let preference = document.createElement("preference");
+      preference.setAttribute("id", prefID);
+      preference.setAttribute("name", "extensions.socialite.sites."+this.siteID+"."+prefName);
+      preference.setAttribute("type", prefType);    
+      this.preferences.appendChild(preference);
+      return preference;
+    },
     
-    // Initial validation and form setup
-    this.validate();
-  },
-  
-  _addBaseSitePreference: function SSProps_addBaseSitePreference(prefID, prefName, prefType) {
-    let preference = document.createElement("preference");
-    preference.setAttribute("id", prefID);
-    preference.setAttribute("name", "extensions.socialite.sites."+this.siteID+"."+prefName);
-    preference.setAttribute("type", prefType);    
-    this.preferences.appendChild(preference);
-    return preference;
-  },
-  
-  addSitePreference: function SSProps_addSitePreference(prefID, prefName, prefType) {
-    return this._addBaseSitePreference(prefID, this.siteClass.prototype.siteClassID+"."+prefName, prefType);
-  },
-  
-  onAccept: function SSProps_onAccept(event) {
-    if (this.isNewSite) {
-      // Apparently, any XMLHttpRequests called from this method "disappear" and never call their callbacks because the window closes immediately.
-      // Some sites may make XMLHttpRequests (particularly for authentication) when they are loaded, so we must ensure XMLHttpRequests work.
-      // Thus, we'll return the information required to create and load the site to the parent preferences window and create the site there.
-      this.newSiteInfo["siteClassID"] = this.prefSiteClassID.value;
-      this.newSiteInfo["siteID"] = this.siteID;
-      this.newSiteInfo["siteName"] = this.prefSiteName.value;
-      this.newSiteInfo["siteURL"] = this.prefSiteURL.value;
-      return true;
-    } else {
-      let needsReload = false;
-      
-      this.site.siteName = this.prefSiteName.value;
-      
-      let newSiteURL = IOService.newURI(this.prefSiteURL.value, null, null).spec;
-      if (this.site.siteURL != newSiteURL) {
-        this.site.siteURL = newSiteURL;
-        needsReload = true;
+    addSitePreference: function SSProps_addSitePreference(prefID, prefName, prefType) {
+      return this._addBaseSitePreference(prefID, this.siteClass.prototype.siteClassID+"."+prefName, prefType);
+    },
+    
+    onAccept: function SSProps_onAccept(event) {
+      if (this.isNewSite) {
+        // Apparently, any XMLHttpRequests called from this method "disappear" and never call their callbacks because the window closes immediately.
+        // Some sites may make XMLHttpRequests (particularly for authentication) when they are loaded, so we must ensure XMLHttpRequests work.
+        // Thus, we'll return the information required to create and load the site to the parent preferences window and create the site there.
+        this.newSiteInfo["siteClassID"] = this.prefSiteClassID.value;
+        this.newSiteInfo["siteID"] = this.siteID;
+        this.newSiteInfo["siteName"] = this.prefSiteName.value;
+        this.newSiteInfo["siteURL"] = this.prefSiteURL.value;
+        return true;
+      } else {
+        let needsReload = false;
+        
+        this.site.siteName = this.prefSiteName.value;
+        
+        let newSiteURL = IOService.newURI(this.prefSiteURL.value, null, null).spec;
+        if (this.site.siteURL != newSiteURL) {
+          this.site.siteURL = newSiteURL;
+          needsReload = true;
+        }
+        
+        if (needsReload) {
+          Socialite.sites.reloadSite(this.site); 
+        }
+        
+        return true;
       }
-      
-      if (needsReload) {
-        Socialite.sites.reloadSite(this.site); 
+    },
+    
+    onCancel: function SSProps_onCancel(event) {
+      // If we we're cancelling adding a new site, we need to release the ID we requested
+      if (this.isNewSite) {
+        Socialite.sites.releaseID(this.siteID);
+        return true;
       }
-      
-      return true;
-    }
-  },
-  
-  onCancel: function SSProps_onCancel(event) {
-    // If we we're cancelling adding a new site, we need to release the ID we requested
-    if (this.isNewSite) {
-      Socialite.sites.releaseID(this.siteID);
-      return true;
-    }
-  },
-  
-  validate: function SSProps_validate(event) {
-    if ((this.textboxSiteName.value == "") || (this.textboxSiteURL.value == "")) {
-      // Short circuit case if either field is empty
-      this.labelSiteURLError.value = "";
-      this.buttonAccept.disabled = true;
-    } else {
-      let validURL;
-      try {
-        IOService.newURI(this.textboxSiteURL.value, null, null);
-        validURL = true;
-      } catch (e if e.name=="NS_ERROR_MALFORMED_URI") {
-        // We were unable to create a URI, so the URL is invalid.
-        validURL = false;
-      }
-      
-      if (!validURL) {
-        this.labelSiteURLError.value = this.strings.getString("siteURLInvalidError");
+    },
+    
+    validate: function SSProps_validate(event) {
+      if ((this.textboxSiteName.value == "") || (this.textboxSiteURL.value == "")) {
+        // Short circuit case if either field is empty
+        this.labelSiteURLError.value = "";
         this.buttonAccept.disabled = true;
       } else {
-        this.labelSiteURLError.value = "";
-        this.buttonAccept.disabled = false;
+        let validURL;
+        try {
+          IOService.newURI(this.textboxSiteURL.value, null, null);
+          validURL = true;
+        } catch (e if e.name=="NS_ERROR_MALFORMED_URI") {
+          // We were unable to create a URI, so the URL is invalid.
+          validURL = false;
+        }
+        
+        if (!validURL) {
+          this.labelSiteURLError.value = this.strings.getString("siteURLInvalidError");
+          this.buttonAccept.disabled = true;
+        } else {
+          this.labelSiteURLError.value = "";
+          this.buttonAccept.disabled = false;
+        }
       }
     }
-  }
-
-};
-var SSP = SocialiteSiteProperties;
+  
+  };
+  
+  var SSProps = SocialiteSiteProperties;
+  return SSProps;
+})();
